b68b8772ee236f1a67573f33ce5abaa5222f0780
Arun-0412 <arunmozhi@apptomate.co>
2023-07-21 18:25:46 +0530

    custom api folders

---
+++ app/code/apptomate/customer/Api/CustomInterface.php
@@ -0,0 +1,12 @@
+<?php
+namespace apptomate\customer\Api; 
+interface CustomInterface
+{
+    /**
+     * GET for Post api
+     * @param string $value
+     * @return string
+     */
+ 
+    public function getData($value);
+}
